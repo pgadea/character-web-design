d93ed8029ac973f4094d819de3de2ba901ea7c56
Pierre <pgadea7@gmail.com>
2018-04-09 22:45:34 -0500

    completed design

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPopup() {
+    let popup = document.getElementById('popup');
+    popup.classList.add('show')
+}
+
+function hidePopup() {
+    let popup = document.getElementById('popup');
+    popup.classList.remove('show')
+}
